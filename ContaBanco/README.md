f9bfc380bfb7820c89bf81ee10bc1bc8f02efa51
srcmatheus <matheusgamarra009@gmail.com>
2025-07-05 06:49:46 -0400

    projeto finalizado

---
+++ ContaBanco/README.md
@@ -0,0 +1,6 @@
+# Projeto Java Conta Banco
+
+---
+
+Desafio simulando uma conta bancária através do terminal/Console.  
+O intuito do projeto é praticar os fundamentos da linguagem e noções de regras de estrutura de sintaxe.
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+//importando a classe Scanner para ler valores pelo terminal.
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        //Criando uma variável para ler valores pelo terminal utilizando a classe Scanner.
+        Scanner scanner = new Scanner(System.in);
+
+        //Solicitando nome e atribuindo o valor a uma variável.
+        System.out.println("Digite seu nome:");
+        String name = scanner.nextLine();
+
+        //Solicitando agência, convertendo o valor para um número inteiro e atribuindo a uma variável.
+        System.out.println("Por favor, digite o número da sua Agência:");
+        int AgNum = Integer.parseInt(scanner.nextLine());
+
+        //Solicitando conta e atribuindo o valor a uma variável.
+        System.out.println("Por favor, digite o número da sua Conta:");
+        String CCNum = scanner.nextLine();
+
+        //Solicitando saldo, convertendo o valor para um número decimal e atribuindo a uma variável.
+        System.out.println("Digite o valor do seu saldo:");
+        Double balance = Double.parseDouble(scanner.nextLine());
+
+        //Mensagem exibindo todos os dados digitado.
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo de R$%s já está disponível para saque.",
+                name,
+                AgNum,
+                CCNum,
+                balance);
     }
 }
